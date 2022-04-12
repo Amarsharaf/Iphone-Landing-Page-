a9d9714fbf0d675a67a6578f41b68eb5ac8969af
amarsharaf <bingbang874@gmail.com>
2022-04-12 23:27:13 +0200

    done with it

---
+++ logic.js
@@ -0,0 +1,9 @@
+let bigPhoto = document.querySelector(".big")
+let page = document.querySelector(".container")
+
+function changeBigPhoto(src) {
+    bigPhoto.src = src
+}
+function changeColor(color) {
+    page.style.background = color
+}
